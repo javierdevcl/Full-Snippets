05ebde0dab451dc4c3e2c3ca7eb4faeacfe7634f
Yuval Tsabar <yuvalsabar@gmail.com>
2016-04-19 08:07:35 +0300

    Delete Custom Fields and Taxonomies.php

--- Custom Fields and Taxonomies.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$banner_object = get_sub_field('banner_image');
-	$banner_image_url = $banner_object['sizes']['home_banner'];
-	$term_id = get_sub_field('banner_link');
-	$term = get_term_by('id', $term_id, 'product_cat');
-	$term_link = get_term_link($term);
-?>
